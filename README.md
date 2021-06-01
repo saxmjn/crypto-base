571f3659b860e5570b7db0e51cc91ffe868efcb3
Saksham <cedaus97@gmail.com>
2021-06-01 22:07:28 +0530

    dydx and 1inc

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 AAVE FLASHLOAN
 https://docs.aave.com/developers/guides/flash-loans
+https://github.com/aave/aave-js/#deposit
+https://github.com/johngrantuk/aaveFlashLoan
+https://github.com/fifikobayashi/AaveV2-BatchFlashDemo/blob/main/BatchFlashDemo.sol
+
 
 UNILEND FLASHLOAN
 https://docs.unilend.finance/flash-loans
+
+
+UNISWAP
+https://github.com/Austin-Williams/uniswap-flash-swapper
---
+++ _protocols/1inch/IOneSplit.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.8.0;
+
+import "./IERC20.sol";
+
+contract IOneSplit { // interface for 1inch exchange.
+    function getExpectedReturn(
+        IERC20 fromToken,
+        IERC20 toToken,
+        uint256 amount,
+        uint256 parts,
+        uint256 disableFlags
+    )
+        public
+        view
+        returns(
+            uint256 returnAmount,
+            uint256[] memory distribution
+        );
+
+    function swap(
+        IERC20 fromToken,
+        IERC20 toToken,
+        uint256 amount,
+        uint256 minReturn,
+        uint256[] memory distribution,
+        uint256 disableFlags
+    ) public payable;
+}
